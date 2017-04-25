cf7542ec490ac773472ed13f69e9bed90c08078e
Brian Arpaio <arpaio@gmail.com>
2017-04-25 22:56:09 +0000

    create and destroy functionality now works

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -57,8 +57,8 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to tasks_url }
+      format.js
     end
   end
 
